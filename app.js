d389c3555157ab5316d72e8ed73add72614c4cfa
ThePaladinDev <133822824+ThePaladinDev@users.noreply.github.com>
2026-03-31 05:03:09 +0530

    Add logic for dynamic board along with safe guards

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 // Constants
-const DEFAULT_SIZE = 16;
-const MIN_SIZE = 2;
-const MAX_SIZE = 100;
+const DEFAULT_SIZE = 20;
+const MIN_SIZE = 5;
+const MAX_SIZE = 80;
 const RESIZE_FACTOR = 2;
 //
 const RESET = 'RESET';
@@ -11,6 +11,40 @@ const INCREASE = 'INCREASE';
 // Variables
 let currentSize = DEFAULT_SIZE;
 
+function getCurrentSize() {
+  return currentSize;
+}
+
+function setCurrentSize(size) {
+  currentSize = size;
+}
+
+// Helper Functions
+function isSafeSize(size) {
+  return MIN_SIZE <= size && size <= MAX_SIZE;
+}
+
+function getDefaultSize() {
+  return DEFAULT_SIZE;
+}
+
+function getDecreasedSize() {
+  const newSize = getCurrentSize() / RESIZE_FACTOR;
+  return isSafeSize(newSize) ? newSize : MIN_SIZE;
+}
+
+function getIncreasedSize() {
+  const newSize = getCurrentSize() * RESIZE_FACTOR;
+  return isSafeSize(newSize) ? newSize : MAX_SIZE;
+}
+
+function changeSize(changeType) {
+  if (changeType === RESET) return getCurrentSize();
+  else if (changeType === DECREASE) return getDecreasedSize();
+  else if (changeType === INCREASE) return getIncreasedSize();
+  return getDefaultSize();
+}
+
 // Document Element Variables
 const board = document.querySelector('.board');
 const boardSizeInfo = document.querySelector('#current-size-info');
@@ -18,9 +52,52 @@ const resetBtn = document.querySelector('#reset-btn');
 const defaultBtn = document.querySelector('#default-btn');
 const decreaseBtn = document.querySelector('#decrease-btn');
 const increaseBtn = document.querySelector('#increase-btn');
+const controlsContainer = document.querySelector('.controls');
 
 //
 
+function updateCurrentSize(controlTriggered) {
+  let newSize;
+  switch (controlTriggered) {
+    case resetBtn:
+      newSize = changeSize(RESET);
+      break;
+    case defaultBtn:
+      newSize = changeSize(DEFAULT);
+      break;
+    case decreaseBtn:
+      newSize = changeSize(DECREASE);
+      break;
+    case increaseBtn:
+      newSize = changeSize(INCREASE);
+      break;
+    default:
+      newSize = changeSize(DEFAULT);
+  }
+  setCurrentSize(newSize);
+  return;
+}
+
+function updateGameState(btn) {
+  updateCurrentSize(btn);
+  restartGame(getCurrentSize());
+}
+
+function handleControlClick(e) {
+  const btn = e.target.closest('.btn');
+  if (!btn) return;
+  updateGameState(btn);
+}
+
+/*
+ * Handle Player Interactions
+ * (with all buttons)
+ */
+controlsContainer.addEventListener('click', handleControlClick);
+
+/*
+ * Logic for starting a new game
+ */
 function clearBoard() {
   board.replaceChildren();
 }
@@ -52,13 +129,24 @@ function makeNewBoard(size) {
   return;
 }
 
+function manageControlDisabledState(size) {
+  decreaseBtn.disabled = size <= MIN_SIZE;
+  increaseBtn.disabled = MAX_SIZE <= size;
+  return;
+}
+
 function updateBoardSizeInfo(size) {
   boardSizeInfo.textContent = `${size} x ${size}`;
+}
+
+function restartGame(size) {
+  startGame(size);
 }
 
 function startGame(size) {
   clearBoard();
   makeNewBoard(size);
+  manageControlDisabledState(size);
   updateBoardSizeInfo(size);
 }
 
